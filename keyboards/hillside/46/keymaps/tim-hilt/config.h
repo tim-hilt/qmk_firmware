04639773ffb67f9eca8d7e1c7b5451452b05e6af
Tim Hilt <thilt@in-technology.eu>
2023-06-29 16:30:22 +0200

    master: added third layer

--- keyboards/hillside/46/keymaps/tim-hilt/config.h
+++ keyboards/hillside/46/keymaps/tim-hilt/config.h
@@ -1,14 +1,11 @@
 #pragma once
 
+#define HOLD_ON_OTHER_KEY_PRESS
+
 // Has to be defined so that left side is detected correctly
 #define SPLIT_HAND_MATRIX_GRID_LOW_IS_RIGHT
 
 #define RP2040_FLASH_GD25Q64CS
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET              // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5 // Specify an optional status LED by GPIO number which blinks when entering the bootloader
-
-// Some boards have issues detecting handedness using the vbus voltage.
-// Such as Elite-C v3, but not v3.1 on. Also apparently some ProMicro boards.
-// For those boands, use usb detection instead.
-// #define SPLIT_USB_DETECT
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5       // Specify an optional status LED by GPIO number which blinks when entering the bootloader
--- keyboards/hillside/46/keymaps/tim-hilt/keymap.c
+++ keyboards/hillside/46/keymaps/tim-hilt/keymap.c
@@ -3,33 +3,30 @@
 enum layers {
     _BASE = 0,
     _SYM,
+    _MACRO,
 };
-
-#define LY_SYM MO(_SYM)
-
-// TODO: Think of ways to use this more clever
-#define UNDO LGUI(KC_Z)
-#define CUT  LGUI(KC_X)
-#define COPY LGUI(KC_C)
-#define PASTE LGUI(KC_V)
-#define REDO LGUI(KC_Y)
 
 // TODO: Leader key?
 // TODO: record / replay macros?
 
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-     KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                             KC_J,    KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-     LGUI_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                             KC_M,    KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT,
-     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MUTE,                                KC_MPLY,         KC_K,    KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_END,
-                                                 KC_LCTL, KC_LALT, KC_SPC, LY_SYM,      LT(_SYM, KC_ENT),  LSFT_T(KC_BSPC), KC_LALT, G(S(KC_4))
+     KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                               KC_J,    KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+     LGUI_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                               KC_M,    KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT,
+     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MUTE,                                  KC_MPLY,         KC_K,    KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_END,
+                                                 KC_LCTL, KC_LALT, KC_SPC, MO(_MACRO),    LT(_SYM, KC_ENT),  LSFT_T(KC_BSPC), KC_LALT, G(S(KC_4))
     ),
     [_SYM] = LAYOUT(
      KC_GRV,  KC_EXLM, KC_AMPR, KC_LCBR, KC_RCBR, KC_CIRC,                                         KC_NO,   KC_7,   KC_8, KC_9, KC_COLN, KC_TRNS,
      KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD,                                         KC_NO,   KC_4,   KC_5, KC_6, KC_PLUS, KC_ASTR,
      KC_TRNS, KC_AT,   KC_PERC, KC_LBRC, KC_RBRC, KC_PIPE, KC_TRNS,                       KC_TRNS, KC_0,    KC_1,   KC_2, KC_3, KC_BSLS, KC_HOME,
                                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [_MACRO] = LAYOUT(
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                          KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                          KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+                                        KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO , KC_NO, KC_NO,   KC_NO
     ),
 };
