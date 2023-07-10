1ff03964a2f1ab3c8e9cd82047b5564ba8118c7c
Tim Hilt <thilt@in-technology.eu>
2023-07-10 13:54:59 +0200

    master: update

--- keyboards/hillside/46/keymaps/tim-hilt/keymap.c
+++ keyboards/hillside/46/keymaps/tim-hilt/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MACRO] = LAYOUT(
      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                          KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                          KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
-     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO,   KC_NO,   KC_F2, KC_NO, KC_NO, KC_NO,
                                         KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO , KC_NO, KC_NO,   KC_NO
     ),
 };
