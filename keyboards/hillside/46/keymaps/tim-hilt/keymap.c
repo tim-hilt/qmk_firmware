5bbafcc61e84795659992bb732c9df8e17854384
Tim Hilt <timhilt@live.de>
2023-08-29 20:36:16 +0200

    Introduce layout for Mac

--- keyboards/hillside/46/keymaps/tim-hilt/keymap.c
+++ keyboards/hillside/46/keymaps/tim-hilt/keymap.c
@@ -1,7 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _BASE = 0,
+    _WIN = 0,
+    _MAC,
     _SYM,
     _INT,
     _TEMPLATE,
@@ -16,15 +17,23 @@ enum custom_keycodes {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(
+    [_WIN] = LAYOUT(
      KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                                 KC_J,     KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_MINS,
      LCTL_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                                 KC_M,     KC_N,      KC_E,    KC_I,    KC_O,    MC_QUOT,
      KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX,                                   XXXXXXX,          KC_K,     KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_END,
                                                  XXXXXXX, KC_LALT, KC_SPC, KC_LGUI,        LT(_SYM, KC_ENT),  LSFT_T(KC_BSPC),  MO(_INT), XXXXXXX
     ),
+    [_MAC] = LAYOUT(
+     KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                                 KC_J,     KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+     LGUI_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                                 KC_M,     KC_N,      KC_E,    KC_I,    KC_O,    MC_QUOT,
+     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX,                                   XXXXXXX,          KC_K,     KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_END,
+                                                 XXXXXXX, KC_LALT, KC_SPC, KC_LCTL,        LT(_SYM, KC_ENT),  LSFT_T(KC_BSPC),  KC_LALT, XXXXXXX
+    ),
+    // More info about persisting the default layer (as opposed to DF) can be found at
+    // https://docs.qmk.fm/#/feature_layers?id=switching-and-toggling-layers
     [_SYM] = LAYOUT(
      MC_GRV,  KC_EXLM, KC_AMPR, KC_LCBR, KC_RCBR, MC_CIRC,                                             G(S(KC_S)),   KC_7,   KC_8, KC_9, KC_COLN, KC_MINS,
-     KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, MC_TILD,                                             KC_NO,        KC_4,   KC_5, KC_6, KC_PLUS, KC_ASTR,
+     KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, MC_TILD,                                             DF(_MAC),     KC_4,   KC_5, KC_6, KC_PLUS, KC_ASTR,
      KC_PIPE, KC_AT,   KC_PERC, KC_LBRC, KC_RBRC, KC_UP,   XXXXXXX,                           XXXXXXX, KC_0,         KC_1,   KC_2, KC_3, KC_BSLS, KC_HOME,
                                          XXXXXXX, KC_LEFT, KC_DOWN,   KC_RGHT,      KC_NO,    KC_NO,   KC_NO,        XXXXXXX
     ),
