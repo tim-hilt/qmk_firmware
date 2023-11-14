2fe060e981b56d200d7e352ce8a3ec42612b364d
Tim Hilt <timhilt@live.de>
2023-11-14 01:20:26 +0100

    add bastardkb/tbkmini

---
+++ keyboards/bastardkb/tbkmini/keymaps/tim-hilt/keymap.c
@@ -0,0 +1,109 @@
+#include QMK_KEYBOARD_H
+#include "os_detection.h"
+
+enum layers {
+    _WIN = 0,
+    _MAC,
+    _WINSYM,
+    _MACSYM,
+    _INT,
+    _TEMPLATE,
+};
+
+enum custom_keycodes {
+    MC_QUOT = SAFE_RANGE,
+    MC_GRV,
+    MC_TILD,
+    MC_CIRC,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_WIN] = LAYOUT_split_3x6_3(
+     KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                                 KC_J,     KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+     LCTL_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                                 KC_M,     KC_N,      KC_E,    KC_I,    KC_O,    MC_QUOT,
+     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                                                 KC_K,     KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_END,
+                                                 KC_LALT, KC_SPC, KC_LGUI,                        LT(_WINSYM, KC_ENT),  LSFT_T(KC_BSPC),  MO(_INT)
+    ),
+    [_WINSYM] = LAYOUT_split_3x6_3(
+     MC_GRV,  KC_EXLM, KC_AMPR, KC_LCBR, KC_RCBR, MC_CIRC,                                                G(S(KC_S)),   KC_7,   KC_8, KC_9, KC_COLN, KC_DEL,
+     KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, MC_TILD,                                                G(KC_UP),     KC_4,   KC_5, KC_6, KC_PLUS, KC_ASTR,
+     KC_PIPE, KC_AT,   KC_PERC, KC_LBRC, KC_RBRC, KC_UP,                                                  KC_0,         KC_1,   KC_2, KC_3, KC_BSLS, KC_HOME,
+                                         KC_LEFT, KC_DOWN,   KC_RGHT,                           KC_NO,    G(KC_LEFT), G(KC_RGHT)
+    ),
+    [_MAC] = LAYOUT_split_3x6_3(
+     KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                                    KC_J,     KC_L,      KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+     LGUI_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                                    KC_M,     KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT,
+     KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                                                    KC_K,     KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_END,
+                                                          KC_LALT, KC_SPC, KC_LCTL,        LT(_MACSYM, KC_ENT),  LSFT_T(KC_BSPC),  KC_LALT
+    ),
+    [_MACSYM] = LAYOUT_split_3x6_3(
+     KC_GRV,  KC_EXLM, KC_AMPR, KC_LCBR, KC_RCBR, KC_CIRC,                                             G(S(KC_4)),   KC_7,   KC_8, KC_9, KC_COLN, KC_DEL,
+     KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD,                                             C(KC_UP),        KC_4,   KC_5, KC_6, KC_PLUS, KC_ASTR,
+     KC_PIPE, KC_AT,   KC_PERC, KC_LBRC, KC_RBRC, KC_UP,                                               KC_0,         KC_1,   KC_2, KC_3, KC_BSLS, KC_HOME,
+                                                  KC_LEFT, KC_DOWN,   KC_RGHT,      KC_NO,    KC_NO,   KC_NO
+    ),
+    [_INT] = LAYOUT_split_3x6_3(
+     KC_NO, KC_NO,      KC_NO, KC_NO,      KC_NO, KC_NO,                                            KC_NO,   KC_NO,   RALT(KC_Y), KC_NO, KC_NO,      KC_NO,
+     KC_NO, RALT(KC_Q), KC_NO, RALT(KC_S), KC_NO, KC_NO,                                            KC_NO,   KC_NO,   RALT(KC_5), KC_NO, RALT(KC_P), KC_NO,
+     KC_NO, KC_NO,      KC_NO, KC_NO,      KC_NO, KC_NO,                                            KC_NO,   KC_NO,   KC_F2,      KC_NO, KC_NO,      KC_NO,
+                                                  KC_NO, KC_NO, KC_NO,             KC_NO , KC_LSFT, KC_NO
+    ),
+    [_TEMPLATE] = LAYOUT_split_3x6_3(
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                         KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                         KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                         KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
+                                 KC_NO, KC_NO, KC_NO,                    KC_NO, KC_NO, KC_NO
+    ),
+};
+
+uint32_t my_set_default_layer(uint32_t trigger_time, void *cb_arg) {
+    uint32_t ret = 500;
+
+    switch (detected_host_os()) {
+        case OS_MACOS:
+        case OS_IOS:
+            default_layer_set(1 << _MAC);
+            ret = 0;
+            break;
+        case OS_WINDOWS:
+            default_layer_set(1 << _WIN);
+            ret = 0;
+            break;
+        case OS_UNSURE:
+        default:
+            break;
+    }
+
+    return ret;
+}
+
+void keyboard_post_init_user(void) {
+    defer_exec(100, my_set_default_layer, NULL);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MC_QUOT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_QUOT) SS_TAP(X_SPC));
+            }
+            return false;
+        case MC_GRV:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_GRV) SS_TAP(X_SPC));
+            }
+            return false;
+        case MC_CIRC:
+            if (record->event.pressed) {
+                SEND_STRING("^" SS_TAP(X_SPC));
+            }
+            return false;
+        case MC_TILD:
+            if (record->event.pressed) {
+                SEND_STRING("~" SS_TAP(X_SPC));
+            }
+            return false;
+    }
+    return true;
+}
